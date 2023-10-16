acc1ee07793d95515075595880071f0c7446b079
Angelica P.A.A <Angearcos03@gmail.com>
2023-10-15 21:04:53 -0600

    se separo el codigo html

---
+++ pao.js
@@ -0,0 +1,34 @@
+// script.js
+
+var vowelsReplacement = {
+    'a': 'army',
+    'e': 'euphoria',
+    'i': 'ice',
+    'o': 'outro',
+    'u': 'singer',
+};
+
+function encrypt() {
+    var message = document.getElementById("message").value;
+    if (message) {
+        var result = message.replace(/[aeiou]/g, function(match) {
+            return vowelsReplacement[match] || match;
+        });
+        document.getElementById("result").innerText = result;
+    }
+}
+
+function decrypt() {
+    var message = document.getElementById("message").value;
+    if (message) {
+        // Invierte el proceso de encriptación
+        var result = message;
+        for (var vowel in vowelsReplacement) {
+            if (vowelsReplacement.hasOwnProperty(vowel)) {
+                var regex = new RegExp(vowelsReplacement[vowel], "g");
+                result = result.replace(regex, vowel);
+            }
+        }
+        document.getElementById("result").innerText = result;
+    }
+}
